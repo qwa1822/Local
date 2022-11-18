fd2e8566bf8b14299f290cb69c851be7392499d5
qwa1822 <asdzxc1822@naver.com>
2022-11-18 14:06:29 +0900

    'variable'

---
+++ codingdream/javascript/4.변수/3.js
@@ -0,0 +1,12 @@
+// typeof: 데이터 타입을 확인
+// 값을 타입 문자열로 변환
+
+let variable;
+console.log(typeof variable);
+
+variable='';
+console.log(typeof variable);
+
+
+varialbe=function(){};
+console.log(typeof varialbe);
